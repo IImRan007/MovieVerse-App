033bfd47c6c543ba7e65bcb19343ee6326ef4f12
Mohd Imran <mohd78696321@gmail.com>
2022-11-27 19:49:05 +0530

    Added SearchURL via api

--- src/context/movies/MovieContext.js
+++ src/context/movies/MovieContext.js
@@ -5,20 +5,33 @@ const MovieContext = createContext();
 
 export const MovieProvider = ({ children }) => {
   const [trendingData, setTrendingData] = useState([]);
-  const url = `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`;
+  const [searchMovie, setSearchMovie] = useState([]);
 
   const getTrendingData = async () => {
-    const response = await axios.get(url);
+    const trendingUrl = `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`;
+
+    const response = await axios.get(trendingUrl);
     const data = await response.data.results;
     console.log(data);
     setTrendingData(data);
+  };
+
+  const getSearchedMovies = async (text) => {
+    const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${text}&page=1&include_adult=false`;
+
+    const response = await axios.get(searchUrl);
+    const data = response.data.results;
+    console.log(data);
+    setSearchMovie(data);
   };
 
   return (
     <MovieContext.Provider
       value={{
         trendingData,
+        searchMovie,
         getTrendingData,
+        getSearchedMovies,
       }}
     >
       {children}
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,6 +1,21 @@
 import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import MovieContext from "../context/movies/MovieContext";
 
 const Navbar = () => {
+  const [text, setText] = useState("");
+
+  const { getSearchedMovies } = useContext(MovieContext);
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getSearchedMovies(text);
+  };
+
   return (
     <div className="navbar bg-gray-600">
       <div className="flex-1">
@@ -10,12 +25,21 @@ const Navbar = () => {
       </div>
       <div className="flex-none gap-2">
         <div className="form-control">
-          <input
-            type="text"
-            placeholder="Search"
-            className="input input-bordered"
-          />
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered"
+              value={text}
+              onChange={handleChange}
+            />
+          </form>
         </div>
+      </div>
+      <div>
+        <button className="btn btn-ghost normal-case text-xl ml-2">
+          Search
+        </button>
       </div>
       <div>
         <Link to="/about" className="btn btn-ghost normal-case text-xl ml-2">
