e5650f7e5b05aa64ddde4af2b3a12dbd571c3165
killshotxd <hassanansari211@gmail.com>
2022-12-03 17:22:10 +0530

    Updated UI COLORS
    Updated several Dependencies
    Updated Color Scheme
    Changed font weight and font sizes
    Aligned several UI element Tags

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,36 +2,42 @@ import { Link } from "react-router-dom";
 
 const Category = () => {
   return (
-    <div className="flex mt-7 justify-evenly flex-wrap">
-      <Link to={`/action`}>
-        <button className="btn bg-primary rounded-full">Action</button>
-      </Link>
-      <Link to="/adventure">
-        <button className="btn bg-secondary rounded-full">Adventure</button>
-      </Link>
-      <Link to="/animation">
-        <button className="btn bg-red-700 rounded-full">Animation</button>
-      </Link>
-      <Link to="/comedy">
-        <button className="btn bg-rose-900 rounded-full">Comedy</button>
-      </Link>
-      <Link to="/crime">
-        <button className="btn bg-emerald-800 rounded-full">Crime</button>
-      </Link>
-      <Link to="/documentary">
-        <button className="btn bg-indigo-500 rounded-full">Documentary</button>
-      </Link>
-      <Link to="/drama">
-        <button className="btn bg-fuchsia-600 rounded-full">Drama</button>
-      </Link>
-      <Link to="/horror">
-        <button className="btn bg-blend-darken rounded-full">Horror</button>
-      </Link>
-      <Link to="/scifi">
-        <button className="btn bg-blue-900 rounded-full">
-          Science Fiction
-        </button>
-      </Link>
+    <div>
+      <p className="text-center mt-2 text-3xl font-bold  ">Categories</p>
+
+      <div className="flex mt-7 justify-evenly flex-wrap">
+        <Link to={`/action`}>
+          <button className="btn bg-primary rounded-full">Action</button>
+        </Link>
+        <Link to="/adventure">
+          <button className="btn bg-secondary rounded-full">Adventure</button>
+        </Link>
+        <Link to="/animation">
+          <button className="btn bg-red-700 rounded-full">Animation</button>
+        </Link>
+        <Link to="/comedy">
+          <button className="btn bg-rose-900 rounded-full">Comedy</button>
+        </Link>
+        <Link to="/crime">
+          <button className="btn bg-emerald-800 rounded-full">Crime</button>
+        </Link>
+        <Link to="/documentary">
+          <button className="btn bg-indigo-500 rounded-full">
+            Documentary
+          </button>
+        </Link>
+        <Link to="/drama">
+          <button className="btn bg-fuchsia-600 rounded-full">Drama</button>
+        </Link>
+        <Link to="/horror">
+          <button className="btn bg-blend-darken rounded-full">Horror</button>
+        </Link>
+        <Link to="/scifi">
+          <button className="btn bg-blue-900 rounded-full">
+            Science Fiction
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -32,26 +32,30 @@ const MovieList = ({ item, id }) => {
       <div className="card-body">
         <h2 className="card-title">
           {item.original_title ? item.original_title : item.name}
-          <div className="badge badge-secondary">NEW</div>
+          <div className="badge  bg-purple-900">NEW</div>
         </h2>
         <p>{item.overview}</p>
         {item.release_date && (
-          <div className="card-actions justify-end">
-            <p>Release Date:</p>
-            <div className="badge badge-outline">{item.release_date}</div>
+          <div className="card-actions justify-end items-center">
+            <p className="font-medium">Release Date:</p>
+            <div className="badge badge-outline bg-purple-700 text-white p-3">
+              {item.release_date}
+            </div>
           </div>
         )}
         {item.vote_average && (
-          <div className="card-actions justify-end">
-            <p>Rating:</p>
-            <div className="badge badge-outline">{item.vote_average}</div>
+          <div className="card-actions justify-end items-center">
+            <p className="font-medium">Rating:</p>
+            <div className="badge badge-outline bg-purple-700 text-white p-3">
+              {item.vote_average}
+            </div>
           </div>
         )}
         <div className=" flex justify-center mt-8 ">
           <button>
             {video && (
               <a
-                className="btn btn-wide normal-case text-xl"
+                className="btn btn-wide normal-case text-xl bg-purple-700"
                 href={`https://www.youtube.com/watch?v=${video}`}
                 target="_blank"
                 rel="noreferrer"
